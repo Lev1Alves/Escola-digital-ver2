7b7c7ce9d927998a442bbf05afd75c4a58028ddc
Pedro Manso <98373747+pmanso28@users.noreply.github.com>
2025-05-04 15:03:25 -0300

    Início da ligação

--- edFront/escola-digital/src/app/app.component.ts
+++ edFront/escola-digital/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [AppService]
 })
 export class AppComponent {
   title = 'escola-digital';
